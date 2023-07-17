4d4ec87ebb32e4d8b2bd64c3afde51646cc0b63b
wdssmq <wdssmq@qq.com>
2023-07-17 18:07:54 +0800

    style. pylint 规范

--- bin/base.py
+++ bin/base.py
@@ -1,14 +1,18 @@
 """ 通用函数封装 """
 import os
 
-def fnEmpty(arg):
+# pylint: disable=invalid-name
+
+def fnEmpty():
+    """ 占位空函数 """
     return 1
 # 什么也不做
 
 
-def fnLog(msg="", tip=None, type=""):
+def fnLog(msg="", tip=None, log_type=""):
+    """ 输出信息 """
     if not tip is None:
-        tip = " ← %s" % tip
+        tip = f" ← {tip}"
     else:
         tip = ""
     if isinstance(msg, list):
@@ -24,37 +28,43 @@ def fnLog(msg="", tip=None, type=""):
     if not any(msg):
         print("")
     else:
-        print("_%s%s%s" % (type, msg, tip))
+        print(f"_{log_type}{msg}{tip}")
 # 输出信息
 
 
 def fnBug(msg, tip=None, debug=True):
+    """ 调试信息输出 """
     if debug:
         fnLog(msg, tip, "[debug]")
 # 调试信息输出
 
 
 def fnErr(msg, tip=None):
+    """ 错误信息 """
     fnLog(msg, tip, "_[err]")
 # 错误信息
 
 
 def fnGetDirsInDir(path):
+    """ 获取子文件夹 """
     return [x for x in os.listdir(path) if os.path.isdir(x)]
 # 获取子文件夹
 
 
 def fnGetFilesInDir(path):
+    """ 获取文件夹中的文件 """
     return [x for x in os.listdir(path) if not os.path.isdir(x)]
 # 获取文件夹中的文件
 
 
 def fnGetFilesInDir2(path, ext):
+    """ 获取指定后缀的文件 """
     return [x for x in os.listdir(path) if not os.path.isdir(x) and os.path.splitext(x)[1] == ext]
 # 获取指定后缀的文件
 
 
 def fnGetFileTime(file):
+    """ 获取文件时间 """
     mtime = os.stat(file).st_mtime  # 文件的修改时间
     ctime = os.stat(file).st_ctime  # 文件的创建时间
     return (int(mtime), int(ctime))
