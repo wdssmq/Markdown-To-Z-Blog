0e80739005dcd913ad143e87ea34d14d6d965979
wdssmq <wdssmq@qq.com>
2023-07-18 12:53:31 +0800

    py. logs 记录对比

--- bin/func.py
+++ bin/func.py
@@ -1,6 +1,10 @@
 """ 函数拆分 """
 import os
 import json
+import inspect
+
+from bin.base import fnBug
+
 
 def read_logs(file_path):
     """ 读取日志 """
@@ -11,3 +15,27 @@ def read_logs(file_path):
         result = {}
     return result
 # 读取日志
+
+
+def check_logs(key, file_mtime, logs_info, debug=False):
+    """ 检查是否需要更新 """
+    logs_list = logs_info["list"]
+    logs_changed = logs_info["changed"]
+
+    fnBug(logs_changed, inspect.currentframe().f_lineno, debug)
+    fnBug(key in logs_changed, inspect.currentframe().f_lineno, debug)
+
+    log_msg = ""
+    log_id = 0
+    if key in logs_list:
+        log_data = logs_list[key]
+        log_id = log_data.get("id", 0)
+        log_mtime = log_data.get("mtime", 0)
+
+        if file_mtime > log_mtime:
+            log_msg = "update"
+        else:
+            log_msg = "skip"
+
+    return log_msg, log_id
+# 检查是否需要更新
