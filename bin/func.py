3a289f8b29486ddcaa8624dbb56bc4821638477f
wdssmq <wdssmq@qq.com>
2023-07-18 14:08:38 +0800

    py. git 更新解析及合并

--- bin/func.py
+++ bin/func.py
@@ -3,9 +3,10 @@ import os
 import json
 import inspect
 
-from bin.base import fnEmpty, fnBug
+from bin.base import fnEmpty, fnLog, fnBug
 
 # pylint: disable=import-outside-toplevel
+
 
 def read_logs(file_path):
     """ 读取日志 """
@@ -18,11 +19,15 @@ def read_logs(file_path):
 # 读取日志
 
 
-def update_logs_git(logs_info):
+def update_logs_git(logs_info, debug=False):
     """ 将 git 更新的文件标记为待更新 """
     from bin.md_func import get_md_info
+    fnLog("### 合并 git 更新到 logs")
+    # # 判断 logs_info["changed"] 类型
+    # print(type(logs_info["changed"]))
     if not any(logs_info["changed"]):
         return
+    fnBug(logs_info["changed"], inspect.currentframe().f_lineno, debug)
     for item in logs_info["changed"]:
         if not os.path.splitext(item)[1] == ".md":
             continue
@@ -30,7 +35,7 @@ def update_logs_git(logs_info):
             continue
         (md_name, md_mtime) = get_md_info(os.path.join(os.getcwd(), item))
         fnEmpty(md_mtime)
-        print(md_name)
+        fnBug(md_name, inspect.currentframe().f_lineno, debug)
         if md_name in logs_info["list"].keys():
             logs_info["list"][md_name]["git_update"] = 1
 # 将 git 更新的文件标记为待更新
@@ -39,7 +44,7 @@ def update_logs_git(logs_info):
 def check_logs(key, file_mtime, logs_info, debug=False):
     """ 检查是否需要更新 """
     logs_list = logs_info["list"]
-    logs_changed = logs_info["changed"]
+    # logs_changed = logs_info["changed"]
 
     # fnBug(logs_changed, inspect.currentframe().f_lineno, debug)
     # fnBug(key in logs_changed, inspect.currentframe().f_lineno, debug)
@@ -51,7 +56,7 @@ def check_logs(key, file_mtime, logs_info, debug=False):
         log_id = log_data.get("id", 0)
         log_mtime = log_data.get("mtime", 0)
 
-        fnBug(log_data, inspect.currentframe().f_lineno , debug)
+        fnBug(log_data, inspect.currentframe().f_lineno, debug)
 
         if file_mtime > log_mtime:
             log_msg = "update"
