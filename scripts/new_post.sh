330acee6a2f0fdfb6764431324437ffea6aa9011
wdssmq <wdssmq@qq.com>
2025-07-09 17:52:28 +0800

    chore. 复用之前创建的文件时更新文件内的日期

--- scripts/new_post.sh
+++ scripts/new_post.sh
@@ -2,20 +2,32 @@
 
 cd _posts
 _date=$(date +%Y-%m-%d)
+_datetime=$(date +%Y-%m-%d\ %H:%M:%S)
 _empty=1970-01-01-empty.md
 
-if [ -d *-new-post ]; then mv *-new-post "${_date}-new-post"; fi
-if [ -f *-new-post.md ]; then mv *-new-post.md "${_date}-new-post.md"; fi
+# 处理已存在的目录，重命名并更新其中的日期
+if [ -d *-new-post ]; then
+  mv *-new-post "${_date}-new-post"
+  if [ -f "${_date}-new-post/doc.md" ]; then
+    sed -i "s/date: [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]/date: ${_datetime}/" "${_date}-new-post/doc.md"
+  fi
+fi
+
+# 处理已存在的文件，重命名并更新其中的日期
+if [ -f *-new-post.md ]; then
+  mv *-new-post.md "${_date}-new-post.md"
+  sed -i "s/date: [0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]/date: ${_datetime}/" "${_date}-new-post.md"
+fi
 
 if [ ! -f "${_date}-new-post.md" ]; then
   cp "${_empty}" "${_date}-new-post.md"
-  sed -i "s/{{ date }}/$(date +%Y-%m-%d\ %H:%M:%S)/" "${_date}-new-post.md"
+  sed -i "s/{{ date }}/${_datetime}/" "${_date}-new-post.md"
 fi
 
 if [ ! -d "${_date}-new-post" ]; then
   mkdir "${_date}-new-post"
   cp "${_empty}" "${_date}-new-post/doc.md"
-  sed -i "s/{{ date }}/$(date +%Y-%m-%d\ %H:%M:%S)/" "${_date}-new-post/doc.md"
+  sed -i "s/{{ date }}/${_datetime}/" "${_date}-new-post/doc.md"
 fi
 
 code "${_date}-new-post.md"
